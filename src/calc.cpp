3177fae5ed6ce166bffb7b9802159cf72c1ba659
navops29 <snavneethchandar@gmail.com>
2020-12-09 22:19:08 +0530

    added actual calculator functions

--- src/calc.cpp
+++ src/calc.cpp
@@ -10,7 +10,40 @@
 
 #include <mathlib/mathlib.hpp>
 
-int main() {
-	std::cout << "!!!Hello World!!!" ; //<< std::endl << add(10,20) << " added" << std::endl; // prints !!!Hello World!!!
+int calc(int n1, int n2, char op){
+
+	/*
+	 * Refers to functions defined in mathlib
+	 */
+	switch (op) {
+	case '+':
+		return add(n1, n2);
+	case '-':
+		return sub(n1,n2);
+	case '/':
+		return divide(n1, n2);
+	case '*':
+		return multiply(n1, n2);
+	default:
+		std::cout << "Invalid operand: " << op <<std::endl;
+		exit(2);
+
+	}
+}
+
+int main(int argc, char **argv) {
+	if (argc != 4 ){
+		std::cout << "Error: incorrect num of args. Usage: calculator <num1> <operand> <num2>" << std::endl;
+		exit(1);
+	}
+
+	int num1 = std::stoi(argv[1]);
+	int num2 = std::stoi(argv[3]);
+
+	char op = argv[2][0];
+
+	int result = calc(num1, num2, op);
+	std::cout << num1 << " " << op << " " << num2 << " = " << result << std::endl;
+
 	return 0;
 }
