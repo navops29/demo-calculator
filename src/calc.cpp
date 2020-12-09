bf0afd1a350f1c147dfe8b0d497c7978f5d93c41
navops29 <snavneethchandar@gmail.com>
2020-12-09 22:19:08 +0530

    initial commit

---
+++ src/calc.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : hellocalc.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+#include <mathlib/mathlib.hpp>
+
+int main() {
+	std::cout << "!!!Hello World!!!" ; //<< std::endl << add(10,20) << " added" << std::endl; // prints !!!Hello World!!!
+	return 0;
+}
